b20e7df57f738a974d278387076e6641f8f6cb24
Claudinei Callegari <cadicallegari@gmail.com>
2018-11-20 20:12:45 -0200

    add not 200 test

--- tests/unit/chaos_test.py
+++ tests/unit/chaos_test.py
@@ -12,7 +12,7 @@ class ChaosTest(unittest.TestCase):
         asyncio.set_event_loop(None)
 
     def test_should_add_url_to_product_on_success(self):
-        async def fakefetcher(c, item):
+        async def fakefetcher(c, url):
             return 200, ""
 
         prods = {}
@@ -27,7 +27,7 @@ class ChaosTest(unittest.TestCase):
         self.assertEqual({"fake/url": True}, urls)
 
     def test_should_respect_url_limit(self):
-        async def fakefetcher(c, item):
+        async def fakefetcher(c, url):
             return 200, ""
 
         prods = {}
@@ -40,6 +40,25 @@ class ChaosTest(unittest.TestCase):
 
         self.assertEqual(
             {'pid': ['fake/url-0', 'fake/url-1']},
+            prods
+        )
+
+    def test_should_not_add_url_that_not_return_ok(self):
+        async def fakefetcher(c, url):
+            if "fail" in url:
+                return 404, ""
+            return 200, ""
+
+        prods = {}
+        urls = {}
+        handler = chaos.item_handler_builder(fakefetcher, prods, urls, 2)
+
+        for i, s in enumerate(["fail", "success", "success", "fail", "success"]):
+            raw = '{"productId": "pid", "image":"fake/url-%i-%s"}' % (i, s)
+            self.loop.run_until_complete(handler(None, raw))
+
+        self.assertEqual(
+            {'pid': ['fake/url-1-success', 'fake/url-2-success']},
             prods
         )
 
